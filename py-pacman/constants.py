6f0022548c18412b14f2eeea865133e3fc336755
shard7 <106669955+shard77@users.noreply.github.com>
2022-11-11 01:48:42 +0100

    V1 commit

--- py-pacman/constants.py
+++
@@ -1,4 +0,0 @@
-WIDTH = 900 # Définir la largeur de la fenêtre
-HEIGHT = 950 # Définir la hauteur de la fenêtre
-COLOR = "red" # Définir la couleur des murs
-FPS = 60 # Définir le nombre d'images par seconde max
